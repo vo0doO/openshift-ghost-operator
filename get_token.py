ad068649a25ff024d8ed5c3af0dbea6550a50d96
vo0doo <kirsanov.bvt@gmail.com>
2022-03-11 21:08:54 +0300

    update 2 files and create 1 file

--- get_token.py
+++ get_token.py
@@ -1,7 +1,11 @@
+from distutils.log import ERROR
+
+
 def get_token(token_path, password: str) -> str:
     if not password or not token_path:
         raise OSError("Без пароля токен не расшифровать")
-        return
     with open(token_path, "r") as f:
         token = f.read()
+    if token == "":
+        raise ERROR("С пустым токеном невозможно работать")
     return token
--- main.py
+++ main.py
@@ -1,17 +1,60 @@
+
 import logging
-from cmd import run
 
-from vars import *
-
-logger = logging.getLogger(__name__)
+from cmdl import run
+from utils import *
 
 
-try:
-    run(COMMANDS["login"])
-    POD_NAME = run(COMMANDS["get_pod"]).replace("pod/", "")
-    logger.info(f"Set project {PROJECT_NAME}, work with pod: {POD_NAME}")
-    # run(SYSTEM_COMMAND["local_backup"])
-    run(COMMANDS["sync"])
-    s = ""
-except Exception as e:
-    logger.error(f"{e}")
+def main():
+    logging.basicConfig(filename='logs.log', filemode='a', format=f'{get_time()}:%(levelname)s:%(message)s', level=logging.INFO)
+    logger = logging.getLogger(__name__)
+    try:
+        logger.info("Start program")
+
+        logger.info("Start clear logs")
+        clear_logs(LOGS_FILE_NAME)
+        logger.info("Finish clear logs\n")
+
+        logger.info("Start login to cluster")
+        run(COMMANDS["login_to_cluster"])
+        logger.info("Finish login to cluster\n")
+
+        logger.info("Start set project")
+        run(COMMANDS["set_project"])
+        logger.info(f"Work with project: {PROJECT_NAME}")
+        logger.info("Finish set project\n")
+
+        logger.info("Start set pod name")
+        POD_NAME = run(COMMANDS["get_pod_name"]).replace("pod/", "")
+        logger.info(f"Work with pod: {POD_NAME}")
+        logger.info("Finish set pod name\n")
+        
+        folders_in_pod = run(f"oc exec {POD_NAME} -- ls content/")
+        
+        
+        if folders_in_pod != '':
+            logger.info("Copy not new - we do only backup in it do not copy anything")
+
+            logger.info("Start clear backup")
+            clear_backups(LOCAL_GHOST_BACKUP_PATH)
+            logger.info("Finish clear backup\n")
+
+            logger.info("Start local backup")
+            backup_local_files()
+            logger.info("Finish local backup\n")
+
+            run(COMMANDS["copy_all_ghost_dirs_from_pod_to_local"])
+            return
+        if folders_in_pod == '':
+            logger.info("Copy new - just copy to it, not making backup")
+            run(COMMANDS["copy_all_ghost_content_dirs_from_local_to_pod"])
+            logger.info("Run ghost start")
+            run(COMMANDS["run_ghost"])
+            logger.info("Run ghost complete")
+            return
+    except Exception as e:
+        logger.error(f"{e}")
+
+
+if __name__ == "__main__":
+    main()
