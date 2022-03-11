bef884934dbef1165f62f297844cfbd3c1115e32
vo0doo <kirsanov.bvt@gmail.com>
2022-03-11 12:37:01 +0300

    update vars.py

--- vars.py
+++ vars.py
@@ -1,35 +1,22 @@
-import logging
 import os
-import sys
 import time
-
 from get_token import get_token
-from run_cmd import run_command, run_commands
 
 time = time.asctime().replace(" ", "-")
-logger = logging.getLogger(__name__)
 
-token_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "token")
-pod_content_dir = "/var/lib/ghost/"
-local_content_dir = "c:/Users/vo0/.ghost/"
-local_backups_content_dir = "c:/Users/vo0/.ghost/backups/"
-pod_name=str
 
-system_command = {
-    "login": f"oc login https://api.pro-eu-west-1.openshift.com --token={get_token(token_path=token_path, password='p')}",
-    "get_pod": "oc get pods -l app=ghostis -o name",
-    "cp_from_pod": f"oc cp {local_content_dir} {pod_name}:{pod_content_dir}",
-    "local_backup": f"cp -r {local_content_dir} {local_backups_content_dir}[{time}]",
+TOKEN_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "token")
+POD_CONTENT_PATH = "/var/lib/ghost/"
+LOCAL_CONTENT_DIR = "c:/Users/vo0/.ghost/"
+LOCAL_BACKUP_CONTENT_DIR = "c:/Users/vo0/.ghost/backups/"
+PROJECT_NAME = "sso"
+APP_NAME = "ghostis"
+POD_NAME=str
+
+SYSTEM_COMMAND = {
+    "login": f"oc login https://api.pro-eu-west-1.openshift.com --token={get_token(token_path=TOKEN_PATH, password='p')}",
+    "set_project": f"oc project {PROJECT_NAME}",
+    "get_pod": f"oc get pods -l app={APP_NAME} -o name",
+    "cp_from_pod": f"oc cp {LOCAL_CONTENT_DIR} {POD_NAME}:{POD_CONTENT_PATH}",
+    "local_backup": f"cp -r {LOCAL_CONTENT_DIR} {LOCAL_BACKUP_CONTENT_DIR}[{time}]",
 }
-
-
-if __name__ == "__main__":
-    try:
-        run_command(system_command["login"])
-        pod_name = run_command(system_command["get_pod"]).replace("pod/", "")
-        logger.info(f" {pod_name}")
-        # run_command(system_command["local_backup"])
-        run_command(system_command["sync"])
-        s = ""
-    except Exception as e:
-        logger.error(f"{e}")
